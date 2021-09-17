1e3fa062cac673892b19a510829c7a4d58168044
matt-36 <mattwoods3006@gmail.com>
2021-09-17 23:54:25 +0100

    add typings

--- src/App_ts.ts
+++ src/App_ts.ts
@@ -1,5 +1,17 @@
+// svelte
 import { writable } from 'svelte/store';
 
-export const nextEl = writable(0);
-export const lastEl = writable(1);
-export const transitionY = writable(500);
+// http
+
+// types
+import { communityType } from './ts/types/commune';
+
+export const nextEl = writable<number>(0);
+export const lastEl = writable<number>(1);
+export const transitionY = writable<number>(500);
+
+
+
+
+// start setting up for http
+export const communityStore = writable<communityType[]>([]);
